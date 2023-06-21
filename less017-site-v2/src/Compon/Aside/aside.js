4c93e731eb8aa8880e2932199bcc76077d969cb7
da <da@mail.ru>
2023-06-21 19:16:15 +0300

    получить API_ Дальше/Свернуть_

--- less017-site-v2/src/Compon/Aside/aside.js
+++ less017-site-v2/src/Compon/Aside/aside.js
@@ -1,31 +1,26 @@
-import { useState } from 'react';
-import './aside.css'
+import { useState } from "react";
+import "./aside.css";
 
-function Aside({el}) {
-let [more, setMore] = useState(false)
+function Aside({ el }) {
+  let [more, setMore] = useState(false);
 
-let moreNoMore=()=>{
-  setMore(!more)
-}
+  let moreNoMore = () => {
+    setMore(!more);
+  };
 
-  return ( 
-  <div className="aside-post">
-  
-    <h4> {el.title}</h4>
+  return (
+    <div className="aside-post">
+      <h4> {el.title}</h4>
 
-    <div>{more? el.body : el.body.slice(0,42)} 
+      <div>
+        {more ? el.body : el.body.slice(0, 42)}
 
-      <button className='btn'
-      onClick={ moreNoMore}
-      >
-        {more? 'Свернуть' : '...Далее'} 
-      </button>
-
+        <button className="btn" onClick={moreNoMore}>
+          {more ? "Свернуть" : "...Далее"}
+        </button>
+      </div>
     </div>
-
-  </div>
- 
-   );
+  );
 }
 
 export default Aside;
