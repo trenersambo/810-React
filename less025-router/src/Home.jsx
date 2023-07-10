2b30877a4ac453b3900df849b6ec210dbcf3ef36
da <da@mail.ru>
2023-07-10 09:26:05 +0300

    learn react router v001

---
+++ less025-router/src/Home.jsx
@@ -0,0 +1,12 @@
+ 
+
+function Home() {
+  return ( 
+  <>
+ 
+  <h2> Это стартовая страница  </h2>
+  </>
+   );
+}
+
+export default Home;
---
+++ less025-router/src/Sambo.jsx
@@ -0,0 +1,14 @@
+
+ 
+
+function Sambo() {
+  return ( 
+  <>
+ 
+    <h2>Page: Sambo</h2>
+
+  </>
+   );
+}
+
+export default Sambo;
